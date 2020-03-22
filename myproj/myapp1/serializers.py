8de9e165c6a443e206438bb8da159048c075c126
mamad yarmoghadam <gamerplus7831@yahoo.com>
2020-03-22 18:45:04 +0430

    final edits

--- myproj/myapp1/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Restaurant
-
-
-class RestaurantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Restaurant
-        fields = ['city', 'place_id', 'name', 'rating', 'address', 'photo_ref', 'lng', 'lat', 'type']
--- myproj/myapp1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import GoogleApiRestaurants, api
-
-my_app = 'myapp1'
-urlpatterns = [
-    path('tehran/', GoogleApiRestaurants.as_view()),
-    path('load/', api),
-]
