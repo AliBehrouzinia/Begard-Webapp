6b0b2b7987d23ed51c5db7b0d2ab5fafebe0717b
mohammadym <gamerplus7831@yahoo.com>
2020-03-25 02:44:43 +0430

    Delete the extra urls

--- Begard_Project/Begard_project/Begard/urls.py
+++ Begard_Project/Begard_project/Begard/urls.py
@@ -21,8 +21,6 @@ from rest_framework.authtoken import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('begard_app.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api-token-auth/', views.obtain_auth_token, name='get_user_auth_token'),
     path('rest-auth/', include('rest_auth.urls')),
     path('accounts/', include('allauth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
