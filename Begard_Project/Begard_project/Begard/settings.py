4afc9c83a68a6a5e3ef4afbe6a46ad5b335a2e61
mohammadym <gamerplus7831@yahoo.com>
2020-03-26 06:56:40 +0430

    Delete the base ande session authentication from defult auth classes in settings.py

--- Begard_Project/Begard_project/Begard/settings.py
+++ Begard_Project/Begard_project/Begard/settings.py
@@ -57,8 +57,6 @@ AUTH_USER_MODEL = 'begard_app.BegardUser'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
                'rest_framework.authentication.TokenAuthentication',
-               'rest_framework.authentication.BasicAuthentication',
-               'rest_framework.authentication.SessionAuthentication',
     ),
 
 }
