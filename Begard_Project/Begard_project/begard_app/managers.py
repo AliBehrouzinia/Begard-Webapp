bc8bd25771c8a62d133ef19a69db125fd7f63106
eatmore18 <reza_alidoost@hotmail.com>
2020-03-31 18:27:33 -0400

    Uncomment the plenty lines of managers.py file

--- Begard_Project/Begard_project/begard_app/managers.py
+++ Begard_Project/Begard_project/begard_app/managers.py
@@ -23,12 +23,12 @@ class BegardUserManager(BaseUserManager):
         """
         Create and save a SuperUser with the given email and password.
         """
-      #  extra_fields.setdefault('is_staff', False)
-       # extra_fields.setdefault('is_superuser', True)
-        #extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
 
-        #if extra_fields.get('is_staff') is not True:
-         #   raise ValueError(_('Superuser must have is_staff=True.'))
-        #if extra_fields.get('is_superuser') is not True:
-        #    raise ValueError(_('Superuser must have is_superuser=True.'))
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
         return self.create_user(email, password)
