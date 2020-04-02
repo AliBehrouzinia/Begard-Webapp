007a3b07e4ca7f286ce0bb3a49c3bd888109cfd8
mostafamasoudi <mostafa.msd.1377@gmail.com>
2020-04-02 16:38:42 +0430

    Add SuggestPlan class to view
    Add this class for suggest plan api.

--- Begard_Project/Begard_project/begard_app/views.py
+++ Begard_Project/Begard_project/begard_app/views.py
@@ -1,4 +1,5 @@
-from rest_framework import status, generics
+from django.utils import timezone
+from rest_framework import status, generics, mixins
 from rest_framework import permissions
 
 from rest_framework.views import APIView
@@ -8,6 +9,9 @@ from rest_framework.permissions import IsAuthenticated
 
 from . import models, serializers
 from .permissions import IsOwnerOrReadOnly
+
+from .time_table import TimeTable
+import datetime
 
 permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                       IsOwnerOrReadOnly]
@@ -32,3 +36,49 @@ class SuggestList(generics.ListAPIView):
         queryset += models.Museum.objects.filter(city=city).order_by('-rating')[0:3]
 
         return queryset
+
+
+class SuggestPlan(mixins.ListModelMixin):
+
+    def list(self, request, *args, **kwargs):
+
+        plan_data = {"start_day": request.data["start_day"],
+                     "finish_day": request.data["finish_day"],
+                     "destination_city": kwargs.get("id"),
+                     "user": None}
+
+        new_plan = serializers.SuggestPlanSerializer(plan_data)
+        if not new_plan.is_valid():
+            return Response(new_plan.error_messages)
+
+        self.generate_items(new_plan, new_plan.destination_city,
+                            timezone.datetime(2020, 4, 1, 18, 0),
+                            timezone.datetime(2020, 4, 3, 16, 0))
+
+        result = {'plan': serializers.SuggestPlanSerializer(new_plan)}
+
+        return Response(result, status=status.HTTP_200_OK)
+
+    def generate_items(self, plan, city_id, start_date, finish_date):
+
+        time_table = TimeTable(start_date, finish_date)
+        time_table.create_slots(120, 60)
+
+        places = self.places(city_id)
+
+        for i in time_table.Table.count():
+            time_table.Table[i].place_id = places[i].place_id
+            time_table.Table[i].plan_id = plan
+
+        return time_table.Table
+
+    def places(self, city_id):
+        city = models.City.objects.get(pk=city_id)
+
+        all_place = list(models.Restaurant.objects.filter(city=city).order_by('-rating'))
+        all_place += models.RecreationalPlace.objects.filter(city=city).order_by('-rating')
+        all_place += models.Museum.objects.filter(city=city).order_by('-rating')
+        all_place += models.ShoppingMall.objects.filter(city=city).order_by('-rating')
+        all_place += models.Cafe.objects.filter(city=city).order_by('-rating')
+
+        return all_place
