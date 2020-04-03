986f154a658f63c8829aa22028083036822cfbe1
mostafamasoudi <mostafa.msd.1377@gmail.com>
2020-04-03 12:36:19 +0430

    Add some method in TimeTable class
    Add some method like 'set_places', for choose place for every slot
    in timetable according to tags of slots.

--- Begard_Project/Begard_project/begard_app/time_table.py
+++ Begard_Project/Begard_project/begard_app/time_table.py
@@ -1,7 +1,6 @@
 import enum
 import datetime
 from .models import *
-from .serializers import SuggestPlanSerializer, PlanItemSerializer
 
 
 def calculate_total_hours(start_time, end_time):
@@ -51,8 +50,85 @@ class TimeTable:
             flag = datetime.datetime(self.StartDateTime.year, self.StartDateTime.month, self.StartDateTime.day, 0, 0, 0)
             for s in range(slot_count_per_day):
                 self.Table[day].append(Slot(flag, flag + activity_timedelta))
-                # slots.append(Slot(flag, flag + activity_timedelta))
                 flag = flag + activity_timedelta + rest_timedelta
+
+    def choose(self, slot, places, type_of_place):
+        selected_place = places[type_of_place][0]
+        slot.Place_id = selected_place['place_id']
+        places[type_of_place].remove(selected_place)
+
+    def unavailable_choose_location(self, slot, places):
+        slot.Place_id = "unavailable"
+        return places
+
+    def breakfast_choose_location(self, slot, places):
+        self.choose(slot, places, 'cafe')
+        return places
+
+    def lunch_dinner_choose_location(self, slot, places):
+        self.choose(slot, places, 'restaurant')
+        return places
+
+    def rest_choose_location(self, slot, places):
+        slot.Place_id = "rest"
+        return places
+
+    def museum_choose_location(self, slot, places):
+        self.choose(slot, places, 'museum')
+        return places
+
+    def recreational_choose_location(self, slot, places):
+        self.choose(slot, places, 'recreational_place')
+        return places
+
+    def tourist_attraction_choose_location(self, slot, places):
+        self.choose(slot, places, 'tourist_attraction')
+        return places
+
+    def shopping_mall_choose_location(self, slot, places):
+        self.choose(slot, places, 'shopping_mall')
+        return places
+
+    select_location = {
+        Tags.Unavailable: unavailable_choose_location,
+        Tags.BreakFast: breakfast_choose_location,
+        Tags.Lunch: lunch_dinner_choose_location,
+        Tags.Dinner: lunch_dinner_choose_location,
+        Tags.Rest: rest_choose_location,
+        Tags.Museum: museum_choose_location,
+        Tags.RecreationalPlace: recreational_choose_location,
+        Tags.TouristAttraction: tourist_attraction_choose_location,
+        Tags.ShoppingMall: shopping_mall_choose_location
+    }
+
+    def set_places(self):
+        places = self.top_from_all_models(10)
+        chosen_so_far = {
+            Tags.Museum: 0,
+            Tags.RecreationalPlace: 0,
+            Tags.TouristAttraction: 0,
+            Tags.ShoppingMall: 0
+        }
+
+        for slot in range(len(self.Table[0])):
+            for day in range(len(self.Table)):
+                slot = self.Table[day][slot]
+                tag = self.get_least_used(slot.Tags, chosen_so_far)
+                places = self.select_location[tag](slot, places)
+
+    @staticmethod
+    def top_from_all_models(n):
+        """get top n places from every model in database according to rating"""
+        result = {
+            "restaurant": list(Restaurant.objects.order_by('-rating')[0:n]),
+            "museum": list(Museum.objects.order_by('-rating')[0:n]),
+            "tourist_attraction": list(TouristAttraction.objects.order_by('-rating')[0:n]),
+            "recreational_place": list(RecreationalPlace.objects.order_by('-rating')[0:n]),
+            "cafe": list(Cafe.objects.order_by('-rating')[0:n]),
+            "shopping_all": list(ShoppingMall.objects.order_by('-rating')[0:n]),
+        }
+
+        return result
 
     def tagging(self):
         self.unavailable_tags(self.Table)
@@ -116,3 +192,15 @@ class TimeTable:
                 if not slot.Is_Lock_for_Tagging:
                     slot.Tags.append(Tags.RecreationalPlace)
                     slot.Tags.append(Tags.ShoppingMall)
+
+    def get_least_used(self, tags, chosen_so_far):
+        if len(tags)==1:
+            return tags[0]
+
+        my_list = {}
+        for t in tags:
+            my_list[t] = chosen_so_far[t]
+
+        tag = sorted(my_list, key=lambda items: items[1])[0]
+
+        return tag[0]
