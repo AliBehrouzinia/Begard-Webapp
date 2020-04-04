36e4e341ecdc9a9ff0976a3e50533891b68bb306
mostafamasoudi <mostafa.msd.1377@gmail.com>
2020-04-04 17:34:05 +0430

    Fix bugs and change url
    - Change url to a differrent in urls
    - Fix bugs and clean SuggestPlan class

--- Begard_Project/Begard_project/begard_app/urls.py
+++ Begard_Project/Begard_project/begard_app/urls.py
@@ -5,4 +5,5 @@ my_app = 'begard_app'
 urlpatterns = [
     path('cities/', views.CitiesList.as_view()),
     path('location/<int:id>/', views.SuggestList.as_view()),
+    path('cities/<int:city_id>/suggestion/', views.SuggestPlan.as_view()),
 ]
--- Begard_Project/Begard_project/begard_app/views.py
+++ Begard_Project/Begard_project/begard_app/views.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from datetime import datetime
 from rest_framework import status, generics, mixins
 from rest_framework import permissions
 
@@ -9,9 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from . import models, serializers
 from .permissions import IsOwnerOrReadOnly
-
 from .time_table import TimeTable
-import datetime
 
 permission_classes = [permissions.IsAuthenticatedOrReadOnly,
                       IsOwnerOrReadOnly]
@@ -38,47 +37,24 @@ class SuggestList(generics.ListAPIView):
         return queryset
 
 
-class SuggestPlan(mixins.ListModelMixin):
+class SuggestPlan(APIView):
+    """Get a plan suggestion to user"""
+    def get(self, request, city_id):
 
-    def list(self, request, *args, **kwargs):
+        dest_city = models.City.objects.get(pk=city_id)
+        start_day = datetime.strptime(self.request.data["start_day"], "%Y-%m-%dT%H:%MZ")
+        finish_day = datetime.strptime(self.request.data["finish_day"], "%Y-%m-%dT%H:%MZ")
 
-        plan_data = {"start_day": request.data["start_day"],
-                     "finish_day": request.data["finish_day"],
-                     "destination_city": kwargs.get("id"),
-                     "user": None}
-
-        new_plan = serializers.SuggestPlanSerializer(plan_data)
-        if not new_plan.is_valid():
-            return Response(new_plan.error_messages)
-
-        self.generate_items(new_plan, new_plan.destination_city,
-                            timezone.datetime(2020, 4, 1, 18, 0),
-                            timezone.datetime(2020, 4, 3, 16, 0))
-
-        result = {'plan': serializers.SuggestPlanSerializer(new_plan)}
+        result = self.get_plan(dest_city, start_day, finish_day)
 
         return Response(result, status=status.HTTP_200_OK)
 
-    def generate_items(self, plan, city_id, start_date, finish_date):
+    def get_plan(self, dest_city, start_date, finish_date):
 
         time_table = TimeTable(start_date, finish_date)
         time_table.create_table(120, 60)
+        time_table.tagging()
+        time_table.set_places(dest_city)
+        plan = time_table.get_json_table()
 
-        places = self.places(city_id)
-
-        for i in time_table.Table.count():
-            time_table.Table[i].place_id = places[i].place_id
-            time_table.Table[i].plan_id = plan
-
-        return time_table.Table
-
-    def places(self, city_id):
-        city = models.City.objects.get(pk=city_id)
-
-        all_place = list(models.Restaurant.objects.filter(city=city).order_by('-rating'))
-        all_place += models.RecreationalPlace.objects.filter(city=city).order_by('-rating')
-        all_place += models.Museum.objects.filter(city=city).order_by('-rating')
-        all_place += models.ShoppingMall.objects.filter(city=city).order_by('-rating')
-        all_place += models.Cafe.objects.filter(city=city).order_by('-rating')
-
-        return all_place
+        return plan
