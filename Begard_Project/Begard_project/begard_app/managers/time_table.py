861be5d445e908fc4206225800149db2f1c6824b
mostafamasoudi <mostafa.msd.1377@gmail.com>
2020-04-06 01:01:28 +0430

    Change name of some variables
    - Change name of some class variable according to PEP8
    - Change in get_least_used method and delete unnecessary variables

--- Begard_Project/Begard_project/begard_app/managers/time_table.py
+++ Begard_Project/Begard_project/begard_app/managers/time_table.py
@@ -4,64 +4,63 @@ from ..models import *
 
 
 class Tags(enum.Enum):
-    Unavailable = 1
-    BreakFast = 2
-    Lunch = 3
-    Dinner = 4
-    Rest = 5
-    Museum = 6
-    RecreationalPlace = 7
-    TouristAttraction = 8
-    ShoppingMall = 9
+    unavailable = 1
+    breakfast = 2
+    lunch = 3
+    dinner = 4
+    rest = 5
+    museum = 6
+    recreational_place = 7
+    tourist_attraction = 8
+    shopping_mall = 9
 
 
 class Slot:
     def __init__(self, start, finish):
-        self.Start = start
-        self.Finish = finish
-        self.Place_id = None
-        self.Place_name = None
-        self.Place_location = None
-        self.Plan_id = None
-        self.Tags = []
-        self.Is_Lock_for_Tagging = False
+        self.start_date = start
+        self.finish_date = finish
+        self.place_id = None
+        self.place_name = None
+        self.place_location = None
+        self.tags = []
+        self.is_lock_for_tagging = False
 
 
 class TimeTable:
     def __init__(self, start, finish):
-        self.Table = []
-        self.StartDateTime = start
-        self.FinishDateTime = finish
+        self.table = []
+        self.start_date_time = start
+        self.finish_date_time = finish
 
     def create_table(self, activity_minute, rest_minute):
-        total_days = self.FinishDateTime.day - self.StartDateTime.day + 1
+        total_days = self.finish_date_time.day - self.start_date_time.day + 1
         slot_count_per_day = (int)(1440 / (activity_minute + rest_minute))
 
         activity_timedelta = datetime.timedelta(minutes=activity_minute)
         rest_timedelta = datetime.timedelta(minutes=rest_minute)
 
         for day in range(total_days):
-            self.Table.append([])
-            flag = datetime.datetime(self.StartDateTime.year, self.StartDateTime.month, self.StartDateTime.day, 0, 0, 0)
+            self.table.append([])
+            flag = datetime.datetime(self.start_date_time.year, self.start_date_time.month, self.start_date_time.day, 0, 0, 0)
             for s in range(slot_count_per_day):
-                self.Table[day].append(Slot(flag, flag + activity_timedelta))
+                self.table[day].append(Slot(flag, flag + activity_timedelta))
                 flag = flag + activity_timedelta + rest_timedelta
 
     def set_places(self, dest_city):
         places = self.top_from_all_models(10, dest_city)
 
         chosen_so_far = {
-            Tags.Museum: 0,
-            Tags.RecreationalPlace: 0,
-            Tags.TouristAttraction: 0,
-            Tags.ShoppingMall: 0
+            Tags.museum: 0,
+            Tags.recreational_place: 0,
+            Tags.tourist_attraction: 0,
+            Tags.shopping_mall: 0
         }
 
-        for slot in range(len(self.Table[0])):
-            for day in range(len(self.Table)):
-                s_slot = self.Table[day][slot]
+        for slot in range(len(self.table[0])):
+            for day in range(len(self.table)):
+                s_slot = self.table[day][slot]
 
-                tag_chosen = self.get_least_used(s_slot.Tags, chosen_so_far)
+                tag_chosen = self.get_least_used(s_slot.tags, chosen_so_far)
 
                 tag = tag_chosen[0]
                 chosen_so_far = tag_chosen[1]
@@ -87,15 +86,15 @@ class TimeTable:
 
     def choose(self, slot, places, type_of_place):
         selected_place = places[type_of_place][0]
-        slot.Place_id = selected_place.place_id
-        slot.Place_name = selected_place.name
-        slot.Place_location = (selected_place.lat, selected_place.lng)
+        slot.place_id = selected_place.place_id
+        slot.place_name = selected_place.name
+        slot.place_location = (selected_place.lat, selected_place.lng)
         places[type_of_place].remove(selected_place)
         return places
 
     def unavailable_choose_location(self, slot, places):
-        slot.Place_id = "unavailable"
-        slot.Place_name = "out of trip"
+        slot.place_id = "unavailable"
+        slot.place_name = "out of trip"
         return places
 
     def breakfast_choose_location(self, slot, places):
@@ -105,8 +104,8 @@ class TimeTable:
         return self.choose(slot, places, 'restaurant')
 
     def rest_choose_location(self, slot, places):
-        slot.Place_id = "rest"
-        slot.Place_name = "Resting time"
+        slot.place_id = "rest"
+        slot.place_name = "Resting time"
         return places
 
     def museum_choose_location(self, slot, places):
@@ -122,38 +121,38 @@ class TimeTable:
         return self.choose(slot, places, 'shopping_mall')
 
     select_location = {
-        Tags.Unavailable: unavailable_choose_location,
-        Tags.BreakFast: breakfast_choose_location,
-        Tags.Lunch: lunch_dinner_choose_location,
-        Tags.Dinner: lunch_dinner_choose_location,
-        Tags.Rest: rest_choose_location,
-        Tags.Museum: museum_choose_location,
-        Tags.RecreationalPlace: recreational_choose_location,
-        Tags.TouristAttraction: tourist_attraction_choose_location,
-        Tags.ShoppingMall: shopping_mall_choose_location
+        Tags.unavailable: unavailable_choose_location,
+        Tags.breakfast: breakfast_choose_location,
+        Tags.lunch: lunch_dinner_choose_location,
+        Tags.dinner: lunch_dinner_choose_location,
+        Tags.rest: rest_choose_location,
+        Tags.museum: museum_choose_location,
+        Tags.recreational_place: recreational_choose_location,
+        Tags.tourist_attraction: tourist_attraction_choose_location,
+        Tags.shopping_mall: shopping_mall_choose_location
     }
 
     def tagging(self):
-        self.unavailable_tags(self.Table)
-        self.rest_tags(self.Table)
-        self.breakfast_lunch_dinner_tags(self.Table, 8, 14, 22)
-        self.museum_touristattraction_tags(self.Table)
-        self.recreationalplace_shoppingmall_tags(self.Table)
+        self.unavailable_tags(self.table)
+        self.rest_tags(self.table)
+        self.breakfast_lunch_dinner_tags(self.table, 8, 14, 22)
+        self.museum_touristattraction_tags(self.table)
+        self.recreationalplace_shoppingmall_tags(self.table)
 
     def unavailable_tags(self, table):
-        hour_start_trip = self.StartDateTime.hour
-        hour_finish_trip = self.FinishDateTime.hour
+        hour_start_trip = self.start_date_time.hour
+        hour_finish_trip = self.finish_date_time.hour
 
         for slot in table[0]:
-            if slot.Start.hour < hour_start_trip:
-                slot.Tags.append(Tags.Unavailable)
-                slot.Is_Lock_for_Tagging = True
+            if slot.start_date.hour < hour_start_trip:
+                slot.tags.append(Tags.unavailable)
+                slot.is_lock_for_tagging = True
 
         last = len(table) - 1
         for slot in table[last]:
-            if slot.Finish.hour > hour_finish_trip:
-                slot.Tags.append(Tags.Unavailable)
-                slot.Is_Lock_for_Tagging = True
+            if slot.finish_date.hour > hour_finish_trip:
+                slot.tags.append(Tags.unavailable)
+                slot.is_lock_for_tagging = True
 
     def rest_tags(self, table, intervals_per_day=None):
         if intervals_per_day is None:
@@ -161,75 +160,71 @@ class TimeTable:
 
         for day in table:
             for slot in day:
-                if not slot.Is_Lock_for_Tagging:
+                if not slot.is_lock_for_tagging:
                     for i in intervals_per_day:
-                        if i[0] <= slot.Start.hour <= (i[1] - 1):
-                            slot.Tags.append(Tags.Rest)
-                            slot.Is_Lock_for_Tagging = True
+                        if i[0] <= slot.start_date.hour <= (i[1] - 1):
+                            slot.tags.append(Tags.rest)
+                            slot.is_lock_for_tagging = True
 
     def breakfast_lunch_dinner_tags(self, table, breakfast, lunch, dinner):
         for day in table:
             for slot in day:
-                if not slot.Is_Lock_for_Tagging:
-                    if slot.Start.hour <= breakfast <= slot.Finish.hour:
-                        slot.Tags.append(Tags.BreakFast)
-                        slot.Is_Lock_for_Tagging = True
-                    elif slot.Start.hour <= lunch <= slot.Finish.hour:
-                        slot.Tags.append(Tags.Lunch)
-                        slot.Is_Lock_for_Tagging = True
-                    elif slot.Start.hour <= dinner <= slot.Finish.hour:
-                        slot.Tags.append(Tags.Dinner)
-                        slot.Is_Lock_for_Tagging = True
+                if not slot.is_lock_for_tagging:
+                    if slot.start_date.hour <= breakfast <= slot.finish_date.hour:
+                        slot.tags.append(Tags.breakfast)
+                        slot.is_lock_for_tagging = True
+                    elif slot.start_date.hour <= lunch <= slot.finish_date.hour:
+                        slot.tags.append(Tags.lunch)
+                        slot.is_lock_for_tagging = True
+                    elif slot.start_date.hour <= dinner <= slot.finish_date.hour:
+                        slot.tags.append(Tags.dinner)
+                        slot.is_lock_for_tagging = True
 
     def museum_touristattraction_tags(self, table):
         for day in table:
             for slot in day:
-                if not slot.Is_Lock_for_Tagging:
-                    if slot.Start.hour <= 16:
-                        slot.Tags.append(Tags.Museum)
-                        slot.Tags.append(Tags.TouristAttraction)
+                if not slot.is_lock_for_tagging:
+                    if slot.start_date.hour <= 16:
+                        slot.tags.append(Tags.museum)
+                        slot.tags.append(Tags.tourist_attraction)
 
     def recreationalplace_shoppingmall_tags(self, table):
         for day in table:
             for slot in day:
-                if not slot.Is_Lock_for_Tagging:
-                    slot.Tags.append(Tags.RecreationalPlace)
-                    slot.Tags.append(Tags.ShoppingMall)
+                if not slot.is_lock_for_tagging:
+                    slot.tags.append(Tags.recreational_place)
+                    slot.tags.append(Tags.shopping_mall)
 
     def get_least_used(self, tags, chosen_so_far):
         if len(tags) == 1:
             return tags[0], chosen_so_far
 
-        my_list = {}
-        for t in tags:
-            my_list[t] = chosen_so_far[t]
-
-        tag = sorted(my_list.items(), key=lambda items: items[1])[0]
+        tag = sorted(chosen_so_far.items(), key=lambda items: items[1])[0]
 
         return tag[0], chosen_so_far
 
     def get_json_table(self):
-        table = self.Table
+        table = self.table
         json = {
             'plan': {
-                'start_date': self.StartDateTime,
-                'finish_date': self.FinishDateTime,
+                'start_date': self.start_date_time,
+                'finish_date': self.finish_date_time,
                 'plan_items': []
             }
         }
 
         for day in table:
             for s_slot in day:
-                if not (s_slot.Tags.__contains__(Tags.Unavailable) or s_slot.Tags.__contains__(Tags.Rest)):
+                if not (s_slot.tags.__contains__(Tags.unavailable) or s_slot.tags.__contains__(Tags.rest)):
                     json_slot = {
-                        'start_date': s_slot.Start,
-                        'finish_date': s_slot.Finish,
+                        'start_date': s_slot.start_date,
+                        'finish_date': s_slot.finish_date,
                         'place_info': {
-                            'id': s_slot.Place_id,
-                            'lat': s_slot.Place_location[0],
-                            'lng': s_slot.Place_location[1]
+                            'id': s_slot.place_id,
+                            'lat': s_slot.place_location[0],
+                            'lng': s_slot.place_location[1]
                         },
-                        'place_name': s_slot.Place_name
+                        'place_name': s_slot.place_name
                     }
 
                     json['plan']['plan_items'].append(json_slot)
