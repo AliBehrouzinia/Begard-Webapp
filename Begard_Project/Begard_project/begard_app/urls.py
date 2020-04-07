5452892a2925fa86a13edf8e39e52228f471faa2
mostafamasoudi <mostafa.msd.1377@gmail.com>
2020-04-07 17:22:59 +0430

    Add url for GetUpdateDeletePlanView class

--- Begard_Project/Begard_project/begard_app/urls.py
+++ Begard_Project/Begard_project/begard_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('cities/', views.CitiesListView.as_view()),
     path('cities/<int:id>/', views.SuggestListView.as_view()),
     path('plans/', views.SavePlanView.as_view()),
+    path('plans/<int:id>/', views.GetUpdateDeletePlanView.as_view()),
     path('cities/<int:id>/search/simple/', views.GlobalSearchList.as_view()),
     path('cities/<int:id>/search/advanced/', views.AdvancedSearch.as_view()),
 ]
