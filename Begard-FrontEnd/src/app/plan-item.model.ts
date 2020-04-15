4ce162be8365aeb782836d7ed81f8df66b1d53fd
eatmore18 <reza_alidoost@hotmail.com>
2020-04-15 12:08:25 -0400

    Add the plan-item model to store the plan data

---
+++ Begard-FrontEnd/src/app/plan-item.model.ts
@@ -0,0 +1,9 @@
+export class PlanningItem{
+
+    constructor(
+      public startDate: string,
+      public finishDate: string,
+      public placeName : string,
+      public placeId : string
+    ) {}
+}
