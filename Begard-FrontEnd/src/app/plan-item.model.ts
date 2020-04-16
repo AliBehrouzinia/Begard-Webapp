f46abc4e81e6a992a8fa40eed3446caa37a05432
Ali Behroozi <ali.bhrzinia@gmail.com>
2020-04-17 00:22:24 +0430

    add search result to list

--- Begard-FrontEnd/src/app/plan-item.model.ts
+++ Begard-FrontEnd/src/app/plan-item.model.ts
@@ -1,9 +1,11 @@
+import { Location } from './location';
+
 export class PlanningItem{
 
     constructor(
       public startDate: string,
       public finishDate: string,
       public placeName : string,
-      public placeId : string
+      public placeId : string,
     ) {}
 }
