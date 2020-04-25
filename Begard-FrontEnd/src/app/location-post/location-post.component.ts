6c21dec602fa7463ed5d6ca0de5933d1ca40f94c
eatmore18 <reza_alidoost@hotmail.com>
2020-04-25 08:58:28 -0400

    Add the post class and interface for responce data from backend

--- Begard-FrontEnd/src/app/location-post/location-post.component.ts
+++ Begard-FrontEnd/src/app/location-post/location-post.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { LocationPostService } from './location-post.service';
+import { LocationPostService, PostRes } from './location-post.service';
+
+class Post{
+  constructor(
+    public type : string,
+    public description: string,
+    public imgSrc : string,
+    public placeName:string,
+    public city : string,
+    public userName : string,
+    public userProImgSrc : string,
+    public followingState : string,
+    
+  ){}
+}
 
 @Component({
   selector: 'app-location-post',
@@ -7,11 +21,8 @@ import { LocationPostService } from './location-post.service';
   styleUrls: ['./location-post.component.css']
 })
 export class LocationPostComponent implements OnInit {
-  public userName: string ;
-  public locationName: string;
-  public cityName: string;
-  public description: string;
-  public imgsrc: string;
+
+  public posts : Post[]= [];
   public isLiked: boolean = false;
   centered = false;
   disabled = false;
@@ -21,9 +32,27 @@ export class LocationPostComponent implements OnInit {
   radius: number;
   color: string;
 
-  constructor(private postservice : LocationPostService) { }
+  constructor(private postservice: LocationPostService) { }
 
   ngOnInit(): void {
+    // this.postservice.getPostData().subscribe(resdata => {
+    //   this.setPostData(resdata);
+    // });
+
+  }
+
+  private setPostData(resdata: PostRes[]) {
+    for (var i = 0; i < resdata.length; i++) {
+      this.posts.push(new Post(resdata[i].type,
+        resdata[i].content,
+        resdata[i].image,
+        resdata[i].place_name,
+        resdata[i].destination_city,
+        resdata[i].user_name,
+        resdata[i].user_profile_image,
+        resdata[i].following_state));
+      
+    }
 
   }
   onLike() {
--- Begard-FrontEnd/src/app/location-post/location-post.service.ts
+++ Begard-FrontEnd/src/app/location-post/location-post.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+export interface PostRes { 
+    "id": number,
+    "type": string,
+    "creation_date": string,
+    "content": string,
+    "image": string,
+    "place_id": string,
+    "place_name": string,
+    "rate": string,
+    "user": number,
+    "plan_id": number,
+    "destination_city": string,
+    "user_name": string,
+    "user_profile_image": string,
+    "following_state": string
+}
+
 @Injectable()
 export class LocationPostService {
     constructor( private http : HttpClient) { }
-  
 
-    setUsername() {
-         return this.http.get("https://begardapp.firebaseio.com/username.json");
-
-
-
+    getPostData(){
+       return this.http.get<PostRes[]>("127.0.0.1:8000/posts/?page=1");
     }
+  
 }
