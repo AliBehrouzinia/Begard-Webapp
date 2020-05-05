532c19e7f252c1aea867b82d44dd27d25e7c8d8d
mostafamasoudi <mostafa.msd.1377@gmail.com>
2020-05-05 19:35:10 +0430

    Change in some permission classes and delete a useless permission class

--- Begard-BackEnd/Begard_project/begard_app/permissions.py
+++ Begard-BackEnd/Begard_project/begard_app/permissions.py
@@ -2,15 +2,6 @@ from rest_framework import permissions
 from . import models
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return obj.owner == request.user
-
-
 class ActionOnFollowRequestPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
