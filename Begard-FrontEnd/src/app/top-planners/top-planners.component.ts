a03a0c69f843ebe74e174aa2571e168621609e89
Ali Behroozi <ali.bhrzinia@gmail.com>
2020-05-07 16:01:26 +0430

    add top-planners section

---
+++ Begard-FrontEnd/src/app/top-planners/top-planners.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-planners',
+  templateUrl: './top-planners.component.html',
+  styleUrls: ['./top-planners.component.css']
+})
+export class TopPlannersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
