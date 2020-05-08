fdfe7bd93001ecf758802d51c31bb88ed92da61e
mostafamasoudi <mostafa.msd.1377@gmail.com>
2020-05-08 13:21:52 +0430

    Add UserPostsView api
    Add UserPostsView class for get list of posts of a user
    - Add UserPostsView class in views.py
    - Add appropriate url for this api

--- Begard-BackEnd/Begard_project/begard_app/views.py
+++ Begard-BackEnd/Begard_project/begard_app/views.py
@@ -537,3 +537,47 @@ class ProfileDetailsView(generics.RetrieveAPIView):
         data['following_state'] = following_state
 
         return Response(data=data, status=status.HTTP_200_OK)
+
+
+class UserPostsView(generics.ListAPIView):
+    """List of posts of a user"""
+    serializer_class = serializers.ShowPostSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        target_user = get_object_or_404(models.BegardUser, id=self.kwargs.get('id'))
+        source_user = self.request.user
+
+        if target_user.is_public:
+            return models.Post.objects.filter(user=target_user)
+
+        if models.UserFollowing.objects.filter(user_id=source_user, following_user_id=target_user).exists():
+            return models.Post.objects.filter(user=target_user)
+
+        return None
+
+    def get(self, request, *args, **kwargs):
+        target_user = get_object_or_404(models.BegardUser, id=self.kwargs.get('id'))
+        source_user = self.request.user
+
+        self.queryset = self.get_queryset()
+
+        posts = list(self.queryset)
+        serializer_data = ShowPostSerializer(instance=self.queryset, many=True).data
+
+        for i in range(len(self.queryset)):
+            serializer_data[i]['user_name'] = posts[i].user.email
+            serializer_data[i]['user_profile_img'] = posts[i].user.profile_img.url
+            serializer_data[i]['destination_city'] = posts[i].plan_id.destination_city.name
+            serializer_data[i]['number_of_like'] = models.Like.objects.filter(post=posts[i].id).count()
+            serializer_data[i]['is_liked'] = models.Like.objects.filter(post=posts[i].id, user=source_user).exists()
+
+            images = models.Image.objects.filter(post=posts[i].id)
+            serializer_data[i]['images'] = [image.image.url for image in images]
+
+            if models.UserFollowing.objects.filter(user_id=source_user, following_user_id=target_user).exists():
+                serializer_data[i]['following_state'] = 'Following'
+            else:
+                serializer_data[i]['following_state'] = 'Follow'
+
+        return Response(serializer_data, status.HTTP_200_OK)
