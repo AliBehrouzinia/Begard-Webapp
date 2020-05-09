f05f62ca5bc6620a6e94f76f1239ef6f3b747e60
Ali Behroozi <ali.bhrzinia@gmail.com>
2020-05-10 01:18:54 +0430

    add functionality to image-list

--- Begard-FrontEnd/src/app/post-location/post-location.component.ts
+++ Begard-FrontEnd/src/app/post-location/post-location.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-location.component.css']
 })
 export class PostLocationComponent implements OnInit {
-
+  images = []
+  imagePath;
+  message;
+  imgURL;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  preview(files) {
+    if (files.length === 0)
+      return;
+
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.imgURL = reader.result;
+      this.images.push(this.imgURL);
+
+    }
+
+    var file = files[0];
+
+    if (files && file) {
+      var binaryReader = new FileReader();
+      binaryReader.readAsDataURL(file);
+    }
   }
 
 }
