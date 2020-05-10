fd754d7d5e220239948c3d141f222529ed7aae4a
Ali Behroozi <ali.bhrzinia@gmail.com>
2020-05-10 16:56:16 +0430

    add my-plans component base

---
+++ Begard-FrontEnd/src/app/my-plan/my-plan.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
+@Component({
+  selector: 'app-my-plan',
+  templateUrl: './my-plan.component.html',
+  styleUrls: ['./my-plan.component.css']
+})
+export class MyPlanComponent implements OnInit {
+  tiles: Tile[] = [
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
+    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
+  ];
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
+
