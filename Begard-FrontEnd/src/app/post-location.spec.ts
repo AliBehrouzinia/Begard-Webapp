335b6ce05410ff31d5c7952d5360bda428ff8ef0
Ali Behroozi <ali.bhrzinia@gmail.com>
2020-05-12 21:08:56 +0430

    add post-location model base

---
+++ Begard-FrontEnd/src/app/post-location.spec.ts
@@ -0,0 +1,7 @@
+import { PostLocation } from './post-location';
+
+describe('PostLocation', () => {
+  it('should create an instance', () => {
+    expect(new PostLocation()).toBeTruthy();
+  });
+});
