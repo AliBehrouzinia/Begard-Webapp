a68a555571c462658c0845a61208e2f83e892a2a
Ali Behroozi <ali.bhrzinia@gmail.com>
2020-05-12 23:42:07 +0430

    add my locations service

--- Begard-FrontEnd/src/app/my-location.service.ts
+++ Begard-FrontEnd/src/app/my-location.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { take, exhaustMap, map } from 'rxjs/operators'
+import { AuthService } from './auth.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { MyLocation } from './my-location'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyLocationService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private authservice: AuthService) { }
+
+  getMyLocationss(planId) {
+    const url = 'http://127.0.0.1:8000/plans/' + planId + '/locations/';
+
+    return this.authservice.user.pipe(take(1), exhaustMap(user => {
+      var token = 'token ' + user.token;
+      return this.http
+        .get<MyLocation[]>(url, {
+          observe: 'response',
+          headers: new HttpHeaders({ 'Authorization': token })
+        })
+        .pipe(
+          map(res => {
+            console.log("fuckin : " + res.body)
+            return res.body;
+          })
+        );
+    }));
+  }
 }
