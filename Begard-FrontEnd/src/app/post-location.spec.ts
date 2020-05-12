8be3887d862cb4a7caf91337e51efd5609a4b618
Ali Behroozi <ali.bhrzinia@gmail.com>
2020-05-12 21:18:58 +0430

    add post-location model

--- Begard-FrontEnd/src/app/post-location.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PostLocation } from './post-location';
-
-describe('PostLocation', () => {
-  it('should create an instance', () => {
-    expect(new PostLocation()).toBeTruthy();
-  });
-});
--- Begard-FrontEnd/src/app/post-location.ts
+++ Begard-FrontEnd/src/app/post-location.ts
@@ -1,2 +1,19 @@
+export interface Image {
+    image: string;
+}
+
 export class PostLocation {
+    type: string;
+    content: string;
+    place_id: string;
+    place_name: string;
+    image: Image[];
+
+    constructor(type: string, content: string, place_id: string, place_name: string, images: Image[]) {
+        this.type = type;
+        this.content = content;
+        this.place_id = place_id;
+        this.place_name = place_name;
+        this.image = this.image;
+    }
 }
