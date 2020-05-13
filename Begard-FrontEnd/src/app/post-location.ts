f2b87b5a39882647c53449887c45fb604b9190f6
Ali Behroozi <ali.bhrzinia@gmail.com>
2020-05-14 02:39:50 +0430

    Add plan-id field to post-location model

--- Begard-FrontEnd/src/app/post-location.ts
+++ Begard-FrontEnd/src/app/post-location.ts
@@ -6,14 +6,16 @@ export class PostLocation {
     type: string;
     content: string;
     place_id: string;
+    plan_id: string;
     place_name: string;
     image: Image[];
 
-    constructor(type: string, content: string, place_id: string, place_name: string, images: Image[]) {
+    constructor(type: string, content: string, plan_id: string, place_id: string, place_name: string, images: Image[]) {
         this.type = type;
         this.content = content;
         this.place_id = place_id;
         this.place_name = place_name;
         this.image = images;
+        this.plan_id = plan_id;
     }
 }
