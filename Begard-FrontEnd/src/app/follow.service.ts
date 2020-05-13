738c6c16c4cdbbca195ea57b4c667a0111f39ab2
Ali Behroozi <ali.bhrzinia@gmail.com>
2020-05-14 01:51:09 +0430

    Setup Follow service

--- Begard-FrontEnd/src/app/follow.service.ts
+++ Begard-FrontEnd/src/app/follow.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { PostLocation } from './post-location'
+
+export interface FollowRequest{
+  request_to
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class FollowService {
 
-  constructor() { }
-}
+  constructor(private http: HttpClient, private authservice: AuthService) { }
+
+  private sendFollowRequest(followRequest) {
+    const url = 'http://127.0.0.1:8000/followings/requests/';
+
+    this.authservice.user.subscribe(user => {
+      var token = 'token ' + user.token;
+      this.http
+        .post<FollowRequest>(url, JSON.stringify(followRequest), {
+          headers: new HttpHeaders({
+            'Authorization': token,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          })
+        }).toPromise().then()
+    })
+
+  }}
