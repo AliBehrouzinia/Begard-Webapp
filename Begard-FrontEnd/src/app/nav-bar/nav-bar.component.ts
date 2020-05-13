78527e2c5dfd527f785ed0cb26bde1255e0b64f5
eatmore18 <reza_alidoost@hotmail.com>
2020-05-13 06:51:36 -0400

    Connect the noitification to the backend point

--- Begard-FrontEnd/src/app/nav-bar/nav-bar.component.ts
+++ Begard-FrontEnd/src/app/nav-bar/nav-bar.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit , Inject} from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Observable } from '../../../node_modules/rxjs';
 import { AuthService } from '../auth.service'
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from "@angular/platform-browser";
-import { MatDialog,MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { take, exhaustMap } from 'rxjs/operators';
 
 export interface DialogData {
   animal: string;
   name: string;
 }
- 
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
+
 export class NavBarComponent implements OnInit {
 
   animal: string;
@@ -44,57 +47,67 @@ export class NavBarComponent implements OnInit {
     this.authService.logout();
   }
 
-  onNotif(){
-    
+  onNotif() {
+
   }
 
-  openDialog(event :Event): void {
-    
-    
+  openDialog(event: Event): void {
+
+
     const dialogRef = this.dialog.open(NotifComponent, {
       width: '250px',
       height: '300px',
-      data: {name: this.name, animal: this.animal},
+      data: { name: this.name, animal: this.animal },
       position: {
         top: '50px',
-        left:  '70%'
+        left: '70%'
       }
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      
+
       this.animal = result;
     });
   }
 }
 
-interface ReqUser{
-   userName : string;
-   disable: boolean
+interface ReqUser {
+  id: number,
+  request_from: number,
+  date: string,
+  profile_img: string,
+  username: string
 }
+
 
 @Component({
   selector: 'dialog-overview-example-dialog',
   templateUrl: './dialog-overview-example-dialog.html',
 })
-export class NotifComponent {
+export class NotifComponent implements OnInit {
   imgUrl = 'https://material.angular.io/assets/img/examples/shiba1.jpg';
-  items : ReqUser[]=[{userName: 'eatmore18',disable: false},
-{userName:'alibehruz1',disable: false},
-{userName:'alibehruz2',disable: false},
-{userName:'alibehruz3',disable: false},
-{userName:'alibehruz4',disable: false},
-{userName:'alibehruz5',disable: false},
-{userName:'alibehruz6',disable: false},
-{userName:'alibehruz7',disable: false},
-{userName:'alibehruz8',disable: false}
-];
-onClear(item: ReqUser) {
-  item.disable= true;
-}
+  items: ReqUser[] = [];
+
+  onClear(item: ReqUser) {
+  }
   constructor(
     public dialogRef: MatDialogRef<NotifComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private http: HttpClient,
+    private auth: AuthService) { }
+  ngOnInit() {
+     this.auth.user.pipe(take(1), exhaustMap(user => {
+      var token = 'token ' + user.token;
+      return this.http.get<ReqUser[]>('http://127.0.0.1:8000/followers/requests/',
+        {
+          headers: new HttpHeaders({ 'Authorization': token })
+        }
+      );
+    })).subscribe(res => {
+       console.log(res);
+    });
+
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
