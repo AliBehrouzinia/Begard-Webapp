a03387aafc7ba9ce9af9fc67e2b155c8d0323554
Ali Behroozi <ali.bhrzinia@gmail.com>
2020-05-14 19:03:04 +0430

    Improve My-plan design

--- Begard-FrontEnd/src/app/my-plan/my-plan.component.ts
+++ Begard-FrontEnd/src/app/my-plan/my-plan.component.ts
@@ -3,6 +3,7 @@ import { MyPlanService } from './../my-plan.service'
 import { ProfileService } from './../profile.service'
 import { MyPlan } from '../my-plan';
 import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 
 
@@ -20,20 +21,21 @@ export class MyPlanComponent implements OnInit {
   followersCount;
   followingCount;
 
-  //todo : get this from api
-  USER_ID = 8;
 
-  constructor(private myPlanService: MyPlanService, private profileService: ProfileService, private router: Router, private route: ActivatedRoute,
+  constructor(private myPlanService: MyPlanService, private profileService: ProfileService
+    , private router: Router, private route: ActivatedRoute, private user: UserService
   ) { }
 
   ngOnInit(): void {
-
-    this.profileService.getProfile(this.USER_ID).subscribe(profile => {
-      this.username = profile.username;
-      this.profileImage = profile.profile_image;
-      this.followersCount = profile.followers_count;
-      this.followingCount = profile.followings_count;
+    this.user.getUserId().subscribe(user => {
+      this.profileService.getProfile(user.pk).subscribe(profile => {
+        this.username = profile.username;
+        this.profileImage = this.SERVER_URL + profile.profile_image;
+        this.followersCount = profile.followers_count;
+        this.followingCount = profile.followings_count;
+      })
     })
+
 
     this.myPlanService.getMyPlans().subscribe(myPlans => {
       this.plansCount = myPlans.length;
