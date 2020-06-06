b774bffec3b9b17b0a16980a5d343b23626146c3
Ali Behroozi <ali.bhrzinia@gmail.com>
2020-06-06 15:23:53 +0000

    Fix/v1.2.0/fix functionality bugs

--- Begard-FrontEnd/src/app/map/location.service.ts
+++ Begard-FrontEnd/src/app/map/location.service.ts
@@ -35,8 +35,13 @@ export class LocationService {
         for (var i = 0; i < planitems.length; i++) {
             this.locations.push({ lan: planitems[i].place_info.lng, lat: planitems[i].place_info.lat, place_name: planitems[i].place_name });
         }
-
     }
 
+    public addLocation(planitem: PlanItem) {
+        this.locations.push({ lan: planitem.place_info.lng, lat: planitem.place_info.lat, place_name: planitem.place_name })
+    }
 
+    public removeLocation(indx) {
+        this.locations.splice(indx, 1)
+    }
 }
